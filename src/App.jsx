7cc685543fede5b6a32e25c09320755bfc9fa7d7
sjbgit <sjb_strat@hotmail.com>
2022-12-17 01:58:14 +0000

    trans list

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import './App.css'
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <div className='container'>
         <Balance />
         <IncomeExpenses /> 
+        <TransactionList/>
       </div>
       
     </>
